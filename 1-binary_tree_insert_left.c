1deaaa8bd7659b023c5240420ca94f0c49d7f1d0
jjenus <biz.jenus@gmail.com>
2022-11-15 13:07:40 +0000

    fixed to betty compliant

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,17 +13,18 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	if (value == '\0')
 		return (NULL);
-	
+
 	binary_tree_t *new_node = binary_tree_node(parent, value);
-	
+
 	if (!new_node)
 		return (NULL);
-	if (parent->left != NULL){
+	if (parent->left != NULL)
+	{
 		new_node->left = parent->left;
 		new_node->left->parent = new_node;
 	}
-	
+
 	parent->left = new_node;
-	
+
 	return (new_node);
 }
